98806124e54f247c32e744afe89659a2d142d617
Shujaat Azim <shujaat.azim@gmail.com>
2020-10-29 20:38:27 -0400

    Removed unused files, and began refactoring to ES6 and modern react principles.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import List from "./Components/List";
+import React from 'react'
+import List from "./Components/List"
 
-function App() {
+const App = () => {
+
   return (
     <div style={{ textAlign: "center" }}>
       <h1>To Do List</h1>
       <h6>By Shujaat Azim</h6>
-    <List />
-  </div>
-  );
+      <List />
+    </div>
+  )
 }
 
 export default App;

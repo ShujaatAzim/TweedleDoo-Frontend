dab1e933059ad58d6c8d05fa8f218388e2501d84
Shujaat Azim <shujaat.azim@gmail.com>
2020-12-10 23:58:03 -0500

    Created Home page component, which will be the main top level app after App gets all the routes.

---
+++ src/MainComponents/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+//will serve as new top level app, after App becomes router base
+
+const Home = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Home

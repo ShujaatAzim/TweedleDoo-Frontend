b570a93847b494a5bf3ee550a57ede4c8c898764
Shujaat Azim <shujaat.azim@gmail.com>
2020-12-18 23:49:24 -0500

    Added some more styliing.

--- src/MainComponents/Home.js
+++ src/MainComponents/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Menu } from 'semantic-ui-react'
 
 //will serve as new top level app, after App becomes router base
 
 const Home = () => {
   return (
     <div>
-
+      <Menu />
     </div>
   )
 }

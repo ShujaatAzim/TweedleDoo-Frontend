988e2a6a368e37349d646372a54637255aa33128
Shujaat Azim <shujaat.azim@gmail.com>
2021-03-24 23:59:28 -0400

    Changed buttons.

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -7,6 +7,13 @@ const LoginPage = () => {
 
   return (
     <div>
+      <Button.Group>
+        <Button type="button" positive>Home</Button>
+        <Button.Or />
+        <Button type="button" positive>About</Button>
+        <Button.Or />
+        <Button type="button" positive>Register</Button>
+      </Button.Group>
       <button onClick={() => history.push("/home")}>Home (test)</button>
       <button onClick={() => history.push("/about")}>About</button>
       <button onClick={() => history.push("/register")}>Register</button>

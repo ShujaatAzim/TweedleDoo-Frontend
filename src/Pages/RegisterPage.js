f3bc943c07f00c23a550f82ff95c3b3d64f39121
Shujaat Azim <shujaat.azim@gmail.com>
2021-04-07 19:54:41 -0400

    Built out registration page, allowing users to be created on the backend through proper auth. Also managed to send the token back to the front end.

--- src/Pages/RegisterPage.js
+++ src/Pages/RegisterPage.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const RegisterPage = () => {
+
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = (e, username, password) => {
+    e.preventDefault();
+    let newUser = { 
+      "user": {
+       "username": username, 
+       "password": password 
+      }
+    }
+    fetch('http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(newUser)
+    })
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+    .then(() => setUsername(""))
+    .then(() => setPassword(""))
+  }
+
   return (
     <div>
-      Register Page
+      <form onSubmit={e => handleSubmit(e, username, password)}>
+        <label>Email</label>
+        <input type="text" name="username" value={username} onChange={e => setUsername(e.target.value)} />
+        <label>Password</label>
+        <input type="text" name="password" value={password} onChange={e => setPassword(e.target.value)} />
+        <input type="submit" />
+      </form>
     </div>
   );
 }

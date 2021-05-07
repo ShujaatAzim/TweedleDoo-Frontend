7eda7d8d606b28b9b010c9d7f8b01332df2b0de4
Shujaat Azim <shujaat.azim@gmail.com>
2021-05-06 23:58:17 -0400

    Added a list of things for the about page.

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -3,7 +3,12 @@ import React from 'react';
 const AboutPage = () => {
   return (
     <div>
-      <h1>This is the about page</h1>
+      <h1>This is the About Page.</h1>
+      <ul>
+        <li>What problem this was made to solve</li>
+        <li>How it was built</li>
+        <li>Maybe future changes?</li>
+      </ul>
     </div>
   );
 }

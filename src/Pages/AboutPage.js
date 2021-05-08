0295f713e954450061acd8b604d10db36f588e8c
Shujaat Azim <shujaat.azim@gmail.com>
2021-05-07 23:54:24 -0400

    Removed list in favor of p tags on the about page.

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -4,11 +4,9 @@ const AboutPage = () => {
   return (
     <div>
       <h1>This is the About Page.</h1>
-      <ul>
-        <li>What problem this was made to solve</li>
-        <li>How it was built</li>
-        <li>Maybe future changes?</li>
-      </ul>
+        <p>What problem this was made to solve</p>
+        <p>How it was built</p>
+        <p>Maybe future changes?</p>
     </div>
   );
 }

d9d366d348d6a32a28bb57251381872bb915a0e8
Shujaat Azim <shujaat.azim@gmail.com>
2021-06-07 22:36:25 -0400

    adjust url

--- src/urlHelper.js
+++ src/urlHelper.js
@@ -1,1 +1,3 @@
-export default process.env.NODE_ENV === 'production' ? 'https://td-backend.shujaatazim.com' : 'http://localhost:3000'
+const url = process.env.NODE_ENV === 'production' ? 'https://td-backend.shujaatazim.com' : 'http://localhost:3000'
+
+export default url
